10247e424b422e17b4ccb1c39624bdfabb72ff2f
nianny <xiaonianhe@hotmail.com>
2021-06-22 01:14:44 +0800

    Basic exit and start game

---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour
+{
+    public void StartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame(){
+        print("Game Exit");
+        Application.Quit();
+    }
+}
